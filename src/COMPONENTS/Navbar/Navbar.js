0b683132054155464218ed668d2a392e232f01be
vaibhav2321 <vaibhavverma2321@gmail.com>
2023-07-24 13:00:39 +0530

    modified:   src/ASSETS/logo.png 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/COMPONENTS/Navbar/Navbar.css 	modified:   src/COMPONENTS/Navbar/Navbar.js
    Changes not staged for commit:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.css
    	modified:   src/App.js
    	modified:   src/COMPONENTS/Navbar/Navbar.css
    	modified:   src/PAGES/HomePages/Home.js
    
    Untracked files:
    	src/ASSETS/img1.jpg
    	src/ASSETS/pic-2.webp
    	src/COMPONENTS/Banners/

--- src/COMPONENTS/Navbar/Navbar.js
+++ src/COMPONENTS/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar =()=> {
                 <img src={logo} alt='logo' className="logo"/>
 
                 <div className="searchbar">
-                    <input type="text" placeholder="Search for the products and categries" className="search"/>
+                    <input type="text" placeholder="Search for products and categries" className="search"/>
                     <button>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -57,9 +57,9 @@ const Navbar =()=> {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Fresh Fruits</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Fresh Vegetables</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">FAQs</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Privacy Policy</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Terms & Conditions</Dropdown.Item>
       </Dropdown.Menu>
             </Dropdown>
             </div>
