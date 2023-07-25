f88024ebeb0d6d8963e902177846cfc7b17d27e3
vaibhav2321 <vaibhavverma2321@gmail.com>
2023-07-26 01:11:15 +0530

    new file:   src/ASSETS/Non veg.png 	new file:   src/ASSETS/dry fruits.png 	new file:   src/ASSETS/fruits.png 	new file:   src/ASSETS/grocery.png 	new file:   src/ASSETS/lentils.png 	deleted:    src/ASSETS/tomato.png 	new file:   src/ASSETS/vegitables.png 	modified:   src/COMPONENTS/Category/HomeCategories.css 	modified:   src/COMPONENTS/Category/HomeCategories.js 	modified:   src/COMPONENTS/Navbar/Navbar.css

--- src/COMPONENTS/Category/HomeCategories.js
+++ src/COMPONENTS/Category/HomeCategories.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import './HomeCategories.css'
+import vegitables from '../../ASSETS/vegitables.png'
+import grocery from '../../ASSETS/grocery.png'
+import fruits from '../../ASSETS/fruits.png'
+import lentils from '../../ASSETS/lentils.png'
+import Non_veg from '../../ASSETS/Non veg.png'
+import dry_fruits from '../../ASSETS/dry fruits.png'
 const HomeCategories = () => {
   return (
     <div className='homecategories'>
-        <div className='container'></div>
-        <div className='container'></div>
-        <div className='container'></div>
-        <div className='container'></div>
+        <div className='container'>
+          <img src={vegitables} alt="vegitables" width={300} height={373}/>
+          <div className='content'>
+            <h1> vegitables at your doorstep</h1>
+            <span>shop vegitables now</span>
+          </div>
+        </div>
+        <div className='container'>
+          <img src={grocery} alt="grocery"  width={300} height={400}/>
+          <div className='content'>
+            <h1> grocery at your doorstep</h1>
+            <p>shop grocery now</p>
+          </div>
+        </div>
+        <div className='container'>
+          <img src={fruits} alt="fruits"  width={300} height={400} />
+          <div className='content'>
+            <h1> fruits at your doorstep</h1>
+            <p>shop fruits now</p>
+          </div>
+        </div>
+        <div className='container'>
+          <img src={lentils} alt="lentils"  width={300} height={400} />
+          <div className='content'>
+            <h1> lentils at your doorstep</h1>
+            <p>shop lentils now</p>
+          </div>
+        </div>
+        <div className='container'>
+          <img src={Non_veg} alt="Non veg"  width={300} height={400}ju />
+          <div className='content'>
+          <h1> Non-veg at your doorstep</h1>
+            <p>shop Non-veg now</p>
+          </div>
+        </div>
+        <div className='container'>
+          <img src={dry_fruits} alt="dry fruits" width={300} height={418}/>
+          <div className='content'>
+            <h1> dry fruits at your doorstep</h1>
+            <span>shop dry fruits now</span>
+          </div>
+        </div>
     </div>
   )
 }
